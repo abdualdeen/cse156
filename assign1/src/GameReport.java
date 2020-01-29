d298bb2fea6e032d38ad6d007afac5b0ba101dec
Abdullah <ahamad2@unl.edu>
2020-01-28 21:18:27 -0600

    new class

--- assign1/src/GameReport.java
+++ assign1/src/GameReport.java
@@ -1,7 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class GameReport {
 	public static void main(String args[]) {
-		
+		try {
+			Scanner fileGiven = new Scanner(new File(args[0]));
+			while (fileGiven.hasNextLine()) {
+				String 
+			}
+			fileGiven.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Input not found.");
+		}
 	}
 
 }
